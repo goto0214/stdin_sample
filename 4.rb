720758b9b9ee20bbd70eba076c00e5346206cadc
goto0214 <yuusuke0214goto1997@outlook.jp>
2022-03-27 13:46:42 +0000

    [Update]

--- 4.rb
+++ 4.rb
@@ -6,11 +6,13 @@ b = gets.to_i
 puts "３つ目の数字を入力してください"
 c = gets.to_i
 puts "計算方法を決めてください"
-puts "足し算の場合は１、引き算は２、掛け算は３、割り算は４"
+puts "足し算の場合は１、引き算は２、掛け算は３"
 puts "数字を入力してください"
 x = gets.to_i
-if x >= 5
+if x >= 4
   puts "４までの数字を入力してください"
+  puts "足し算の場合は１、引き算は２、掛け算は３"
+  puts "数字を入力してください"
   x = gets.to_i
 end
 
@@ -18,4 +20,6 @@ if x == 1
   puts "a+b+c=#{a + b + c}"
 elsif x == 2
   puts "a-b-c=#{a - b - c}"
+else
+  puts "a*b*c=#{a * b * c}"
 end
