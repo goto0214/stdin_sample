696ac97d8c50e1b0e2567fffd01549a338e286de
goto0214 <yuusuke0214goto1997@outlook.jp>
2022-03-27 15:42:28 +0000

    [Update]

---
+++ fb.rb
@@ -0,0 +1,15 @@
+def FizzBuzz(num)
+  if num % 15 == 0
+    "FizzBuzz"
+  elsif num % 3 == 0
+    "Fizz"
+  elsif num % 5 == 0
+    "Buzz"
+  else
+    num.to_s
+  end
+end
+
+input = gets.to_i
+
+puts FizzBuzz(input)
